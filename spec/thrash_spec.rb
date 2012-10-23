85ea0be8aa793aaf1d3229539db134fc9c21f432
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-10-23 16:40:58 -0400

    basic specs implemented

--- spec/thrash_spec.rb
+++ spec/thrash_spec.rb
@@ -1,7 +1,35 @@
 require 'spec_helper'
 
+TEST_DIR = ENV['TEST_DIR'] || "/tmp/thrash_test_#{(0...8).map{65.+(rand(25)).chr}.join}/"
+
+
 describe Thrash do
+
+  let(:test_dir) { TEST_DIR }
+  let(:thrasher) { Thrash.new }
+  let(:files) { files = 5.times.map { |x| File.join(test_dir, "test-#{x}") } }
+  let(:test_data) { "test" }
+
+  before(:all) { `mkdir -p #{test_dir}` }
+
   it 'can be initialized' do
-    Thrash.new.should_not be_nil
+    thrasher.should_not be_nil
   end
+
+  it '#add accepts data' do
+    files.each do |out_file|
+      thrasher.add(out_file, test_data)
+    end
+  end
+
+  it '#finalize finalizes output and returns nil' do
+    thrasher.finalize.should be_nil
+  end
+
+  it 'writes data as expected' do
+    files.each do |f|
+      File.read(f).should == test_data
+    end
+  end
+
 end
