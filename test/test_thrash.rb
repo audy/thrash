6290602d7c782b9ce6fd846e876e036e2f2fc7f1
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-10-23 15:59:12 -0400

    md

--- test/test_thrash.rb
+++ test/test_thrash.rb
@@ -1,7 +1,2 @@
-require 'helper'
+require 'spec_helper'
 
-class TestThrash < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
-end
